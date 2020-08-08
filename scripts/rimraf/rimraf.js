9cbff6f62b109cfeecaa16e06c1f564ef0aafc9e
iamfotx <ahmad@techxphere.com>
2020-08-08 23:27:46 +0500

    fromat

--- scripts/rimraf/rimraf.js
+++ scripts/rimraf/rimraf.js
@@ -7,16 +7,16 @@ const dirsNameWithinPackages = getChildDirsName(fullPathToPackages)
 const folderNamesToRemove = process.argv.slice(2)
 
 const pathsToDelete = dirsNameWithinPackages
-  .map((dir) =>
-    folderNamesToRemove.map((folderNameToRemove) =>
+  .map(dir =>
+    folderNamesToRemove.map(folderNameToRemove =>
       path.join(dir, folderNameToRemove),
     ),
   )
   .flat()
-  .map((modifiedPath) => path.join(process.cwd(), 'packages', modifiedPath))
+  .map(modifiedPath => path.join(process.cwd(), 'packages', modifiedPath))
 
-pathsToDelete.forEach((pathToDelete) => {
-  rimraf(pathToDelete, {}, (e) => {
+pathsToDelete.forEach(pathToDelete => {
+  rimraf(pathToDelete, {}, e => {
     if (e) {
       throw new Error(e)
     }
