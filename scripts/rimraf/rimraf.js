26e53d41896e8d09f7c793b8bcc5540abbbbc439
iamfotx <ahmad@techxphere.com>
2020-08-08 23:21:33 +0500

    add prettier

--- scripts/rimraf/rimraf.js
+++ scripts/rimraf/rimraf.js
@@ -6,9 +6,19 @@ const fullPathToPackages = path.join(process.cwd(), 'packages')
 const dirsNameWithinPackages = getChildDirsName(fullPathToPackages)
 const folderNamesToRemove = process.argv.slice(2)
 
-const pathsToDelete = dirsNameWithinPackages.map(dir => folderNamesToRemove.map(folderNameToRemove => `${dir}/${folderNameToRemove}`)
-).flat().map(modifiedPath => path.join(process.cwd(), 'packages', modifiedPath))
+const pathsToDelete = dirsNameWithinPackages
+  .map((dir) =>
+    folderNamesToRemove.map((folderNameToRemove) =>
+      path.join(dir, folderNameToRemove),
+    ),
+  )
+  .flat()
+  .map((modifiedPath) => path.join(process.cwd(), 'packages', modifiedPath))
 
-pathsToDelete.forEach(pathToDelete => {
-  rimraf(pathToDelete, {}, (e) => console.log(e))
+pathsToDelete.forEach((pathToDelete) => {
+  rimraf(pathToDelete, {}, (e) => {
+    if (e) {
+      throw new Error(e)
+    }
+  })
 })
--- scripts/shared/get-child-dirs-name.js
+++ scripts/shared/get-child-dirs-name.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 
 function getChildDirsName(path) {
   return fs.readdirSync(path).filter(function (file) {
-    return fs.statSync(path+'/'+file).isDirectory();
-  });
+    return fs.statSync(path + '/' + file).isDirectory()
+  })
 }
 
 module.exports = getChildDirsName
