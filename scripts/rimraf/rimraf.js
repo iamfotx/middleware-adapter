5357797704642b8623c0bba4bb797634bd8479fc
iamfotx <ahmad@techxphere.com>
2020-08-08 21:27:11 +0500

    add lerna & scripts

---
+++ scripts/rimraf/rimraf.js
@@ -0,0 +1,14 @@
+const rimraf = require('rimraf')
+const path = require('path')
+const getChildDirsName = require('../shared/get-child-dirs-name')
+
+const fullPathToPackages = path.join(process.cwd(), 'packages')
+const dirsNameWithinPackages = getChildDirsName(fullPathToPackages)
+const folderNamesToRemove = process.argv.slice(2)
+
+const pathsToDelete = dirsNameWithinPackages.map(dir => folderNamesToRemove.map(folderNameToRemove => `${dir}/${folderNameToRemove}`)
+).flat().map(modifiedPath => path.join(process.cwd(), 'packages', modifiedPath))
+
+pathsToDelete.forEach(pathToDelete => {
+  rimraf(pathToDelete, {}, (e) => console.log(e))
+})
---
+++ scripts/shared/get-child-dirs-name.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+
+function getChildDirsName(path) {
+  return fs.readdirSync(path).filter(function (file) {
+    return fs.statSync(path+'/'+file).isDirectory();
+  });
+}
+
+module.exports = getChildDirsName
