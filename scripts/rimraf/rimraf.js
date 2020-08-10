50c1fb7e8215199bf7ef6205cac4bbc604c078c5
iamfotx <ahmad@techxphere.com>
2020-08-10 12:05:21 +0500

    fix rimraf recursive dir issue

--- scripts/rimraf/rimraf.js
+++ scripts/rimraf/rimraf.js
@@ -2,20 +2,35 @@ import rimraf from 'rimraf'
 import path from 'path'
 import {getChildDirsName} from '../shared/get-child-dirs-name.js'
 
-const fullPathToPackages = path.join(process.cwd(), 'packages')
-const dirsNameWithinPackages = getChildDirsName(fullPathToPackages)
-const folderNamesToRemove = process.argv.slice(2)
+const rootDir = process.cwd()
+const foldersToRemove = process.argv.slice(2)
 
-const pathsToDelete = dirsNameWithinPackages
-  .map(dir =>
-    folderNamesToRemove.map(folderNameToRemove =>
-      path.join(dir, folderNameToRemove),
-    ),
-  )
+// Dirs to clean
+const packages = path.join(rootDir, 'packages')
+const scripts = path.join(rootDir, 'scripts')
+
+const pathsToClean = [packages, scripts]
+  .map(dir => {
+    const childDirs = getChildDirsName(dir)
+    return childDirs.map(childDir => {
+      return path.join(dir, childDir)
+    })
+  })
   .flat()
-  .map(modifiedPath => path.join(process.cwd(), 'packages', modifiedPath))
 
-pathsToDelete.forEach(pathToDelete => {
+// clean the root also
+pathsToClean.push(rootDir)
+
+// paths to run rimraf on
+const actualPathsToDelete = pathsToClean
+  .map(dir => {
+    return foldersToRemove.map(folder => {
+      return path.join(dir, folder)
+    })
+  })
+  .flat()
+
+actualPathsToDelete.forEach(pathToDelete => {
   rimraf(pathToDelete, {}, e => {
     if (e) {
       throw new Error(e)
